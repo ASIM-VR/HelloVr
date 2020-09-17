d1910dab3c352e571fbeca8da09a38237740cec8
Nupsi <nupsi4.thenoob@gmail.com>
2020-09-17 05:07:05 +0300

    Initial commit

---
+++ Assets/ASIM/Script/GUI/FloatSlider.cs
@@ -0,0 +1,31 @@
+﻿using AsimVr.Parameters;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AsimVr.GUI
+{
+    [RequireComponent(typeof(Slider))]
+    public class FloatSlider : MonoBehaviour
+    {
+        [SerializeField]
+        private FloatParameter m_parameter;
+
+        private Slider m_slider;
+
+        private void Awake()
+        {
+            m_slider = GetComponent<Slider>();
+            m_slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnEnable()
+        {
+            m_slider.value = m_parameter.Value;
+        }
+
+        private void OnValueChanged(float value)
+        {
+            m_parameter.Value = value;
+        }
+    }
+}
---
+++ Assets/ASIM/Script/Parameters/GenericParameter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace AsimVr.Parameters
+{
+    public abstract class GenericParameter<T> : ScriptableObject
+    {
+        [SerializeField]
+        protected T m_value;
+
+        protected Action m_onValueChange;
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if(UnityEditor.EditorApplication.isPlaying)
+            {
+                m_onValueChange?.Invoke();
+            }
+#endif
+        }
+
+        public void AddListener(Action action)
+        {
+            m_onValueChange += action;
+        }
+
+        public void RemoveListener(Action action)
+        {
+            m_onValueChange -= action;
+        }
+
+        public T Value
+        {
+            get => m_value;
+            set
+            {
+                m_value = value;
+                m_onValueChange?.Invoke();
+            }
+        }
+    }
+}
